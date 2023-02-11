6b02e1fbbb3ad925bccfd09f0443bb37bfdf6a13
Attila <Attila@LAPTOP-2UHVGMJE>
2023-02-11 00:17:14 +0000

    Add project files.

---
+++ AdoNet_CRUD2/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNet_CRUD2
+{
+    public class ApplicationDbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public void GetConnectionStringDB()
+        {
+            var getJsonData = _configuration.GetSection("ConnectionStrings:Default").Value;
+            Console.WriteLine($"SQL Server ConnectionString : {getJsonData} ");
+        }
+    }
+}
---
+++ AdoNet_CRUD2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+using AdoNet_CRUD2;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+// build a collection
+var serviceCollection = new ServiceCollection();
+
+// build a configuration
+IConfiguration configuration;
+
+configuration = new ConfigurationBuilder().SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+    .AddJsonFile("appsettings.json").Build();
+
+// add configuration to the service collection
+
+serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+serviceCollection.AddSingleton<ApplicationDbContext>();
+
+// Testing
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+
+var getInstanceDb = serviceProvider.GetService<ApplicationDbContext>();
+getInstanceDb.GetConnectionStringDB();
+
+Console.ReadLine();
